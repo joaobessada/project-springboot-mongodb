90d703788a9f4fdcc538363d2667128fa77a7d4f
joaobessada <joaobessada@gmail.com>
2022-12-01 16:31:48 -0300

    user insert

--- src/main/java/com/bessada/workshopmongo/services/UserService.java
+++ src/main/java/com/bessada/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bessada.workshopmongo.domain.User;
+import com.bessada.workshopmongo.dto.UserDTO;
 import com.bessada.workshopmongo.repository.UserRepository;
 import com.bessada.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,4 +25,12 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not founded!"));
 		}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
