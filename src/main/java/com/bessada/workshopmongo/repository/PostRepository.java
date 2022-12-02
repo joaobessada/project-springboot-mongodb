d774c90610e7406b1c47b2810e3031d08e0b4c75
joaobessada <joaobessada@gmail.com>
2022-12-02 08:17:54 -0300

    @query mongorepository consult

--- src/main/java/com/bessada/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/bessada/workshopmongo/repository/PostRepository.java
@@ -4,11 +4,16 @@ package com.bessada.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bessada.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	//https://www.mongodb.com/docs/manual/reference/operator/query/regex/
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") 
+	List<Post> searchTitle(String text);
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
