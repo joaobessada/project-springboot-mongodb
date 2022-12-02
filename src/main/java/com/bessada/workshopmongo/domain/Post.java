a20350fce3ae2cde4e060468e842e89d5bf99dd2
joaobessada <joaobessada@gmail.com>
2022-12-02 06:37:23 -0300

    post by id

--- src/main/java/com/bessada/workshopmongo/domain/Post.java
+++ src/main/java/com/bessada/workshopmongo/domain/Post.java
@@ -2,8 +2,6 @@ package com.bessada.workshopmongo.domain;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.Objects;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
